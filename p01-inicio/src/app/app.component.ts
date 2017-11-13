3945c6d99652b47ea1fa24bb04893286ced1696b
iMisut <ivandelafuentemisut@gmail.com>
2017-11-13 16:14:20 +0100

    P01 - Inicio

---
+++ p01-inicio/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Gijón';
+  autor = 'Iván de la Fuente Misut';
+  fecha = '2017';
+}
